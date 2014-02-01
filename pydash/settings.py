bd62ead54d1e93b40d5d10724345672adf65a069
Florian N <michael@hostechs.com>
2014-01-31 23:39:21 -0500

    style updates and more

--- pydash/settings.py
+++ pydash/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'pydash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
