ffde36ff1dc17d62e19867762bd0109d7ba05115
Florian N <michael@hostechs.com>
2014-02-17 22:49:39 -0500

    fix ss output

--- main/views.py
+++ main/views.py
@@ -299,12 +299,11 @@ def get_netstat():
     Get ports and applications
     """
     try:
-        pipe = os.popen("ss -tlnp | awk '{print $4, $5, $6}'")
+        pipe = os.popen("ss -tnp | grep ESTAB| awk '{print $4, $5}'")
         data = pipe.read().strip().split('\n')
         pipe.close()
         
         data = [i.split(None, 3) for i in data]
-	del data[0]
     
     except Exception, err:
         data = str(err)
